22b2299df4528919f3e6044feb9582f34adce4a8
AchmadDewa <132963479+AchmadDewa@users.noreply.github.com>
2025-07-25 00:21:00 +0800

    add admin view ♪

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home');
 
+Route::get('/admin', [\App\Http\Controllers\AdminController::class, 'index']);
+
 Route::prefix('products')->group(function () {
     Route::get('/{product}', [App\Http\Controllers\ProductController::class, 'show'])
         ->name('products.show');
